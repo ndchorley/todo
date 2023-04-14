679ed4e0f49994fa15910e2984a6ec8dc44884cd
Chris James <chris.james@springernature.com>
2023-04-14 15:53:53 +0100

    almost working

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,7 +1,8 @@
 (ns todo.core
   (:require
    [ring.adapter.jetty :refer [run-jetty]]
-   [hiccup.page :as page])
+   [hiccup.page :as page]
+   [hiccup.core :refer :all])
   (:gen-class))
 
 (def todos (atom [
@@ -9,19 +10,27 @@
                   {:name "Buy beer" :done false}
                   ]))
 
-(defn render [todos]
+
+(defn render-todos-fragment [todos]
+  [:div {:id "todos"}
+   (map
+     (fn [todo] [:h1 (todo :name)])
+     todos)
+   ])
+
+(defn render-whole-page [todos]
   (page/html5
     [:body
      [:script {:src "https://unpkg.com/htmx.org@1.9.0" :crossorigin "anonymous"}]
-      (map
-        (fn [todo] [:h1 (todo :name)])
-        todos)
-     [:form {:hx-post "/todos"}
+      (render-todos-fragment todos)
+     [:form {:hx-post "/todos" :hx-target "#todos"}
       [:input {:type "text" :name "todo-name"}]
       [:input {:type "submit"}]]
      ]
   )
 )
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -32,8 +41,8 @@
       (if (= (request :request-method) :post)
         (do
           (swap! todos
-                 (fn [todos] (conj todos {:name "lol" :done false})))
-          {:status 200 :body (render (deref todos))})
+                 (fn [todos] (conj todos {:name (request :body) :done false}))) ;Nicky, i don't know how to get form data, but this "works"
+          {:status 200 :body (html (render-todos-fragment (deref todos)))})
 
-        {:status 200 :body (render (deref todos))}))
+        {:status 200 :body (render-whole-page (deref todos))}))
     {:port 8080}))
