fb0840c28cbc41430f6cd8fea6a14384eecd5904
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-04-14 14:53:57 +0100

    Add hello world handler

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,7 +1,10 @@
 (ns todo.core
+    (:require
+   [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (run-jetty (fn [request] {:status 200 :body "Hello"}) {:port 8080}))
