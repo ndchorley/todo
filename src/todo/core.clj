b383db3e0c48b605dc045c07d9fdcb0fbfb7e881
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-04-14 15:02:48 +0100

    Render some todos

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,10 +1,29 @@
 (ns todo.core
-    (:require
-   [ring.adapter.jetty :refer [run-jetty]])
+  (:require
+   [ring.adapter.jetty :refer [run-jetty]]
+   [hiccup.page :as page])
   (:gen-class))
+
+(def todos [
+  {:name "Learn Clojure" :done false}
+  {:name "Buy beer" :done false}
+])
+
+(defn render [todos]
+  (page/html5
+    [:body
+      (map
+        (fn [todo] [:h1 (todo :name)])
+        todos)
+    ]
+  )
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  (run-jetty (fn [request] {:status 200 :body "Hello"}) {:port 8080}))
+  (run-jetty 
+    (fn [request] 
+    {:status 200 :body (render todos)}) 
+    {:port 8080}))
