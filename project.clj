7bbf317e1c0e606b8c46c49489f9faba7f5c0820
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-04-14 14:27:04 +0100

    Add ring to dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [
+    [org.clojure/clojure "1.11.1"]
+    [ring/ring-core "1.9.5"]
+    [ring/ring-jetty-adapter "1.9.5"]
+    [ring/ring-mock "0.4.0"]
+    [hiccup "1.0.5"]]
   :main ^:skip-aot todo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
