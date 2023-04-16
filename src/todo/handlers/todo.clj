4481d8d114ece798a697441cc932b53b210c25dd
Chris James <quii@hey.com>
2023-04-16 08:24:51 +0100

    remove hiccup dependency in handlers

--- src/todo/handlers/todo.clj
+++ src/todo/handlers/todo.clj
@@ -1,10 +1,10 @@
 (ns todo.handlers.todo
-  [:require [todo.views.todo :refer :all]
-            [hiccup.core :refer :all]])
+  [:require [todo.views.todo :refer :all]])
 
 (defn render-index [todos] (render-whole-page todos))
 
-(defn handle-new-todo [req, todos, add-todo] (let [new-todo (:todo-name (:params req))]
-                                            (do
-                                              (add-todo new-todo)
-                                              (-> todos render-todos-fragment html))))
+(defn handle-new-todo [req, todos, add-todo]
+  (let [new-todo (:todo-name (:params req))]
+    (do
+      (add-todo new-todo)
+      (render-todos-fragment todos))))
--- src/todo/views/todo.clj
+++ src/todo/views/todo.clj
@@ -2,18 +2,20 @@
   (:require     [hiccup.page :as page]
                 [hiccup.core :refer :all]))
 
-(defn render-todos-fragment [todos]
+(defn render-todos [todos]
   [:div {:id "todos"}
    (map
      (fn [todo] [:h1 (todo :name)])
      todos)
    ])
 
+(defn render-todos-fragment [todos] (-> todos render-todos html))
+
 (defn render-whole-page [todos]
   (page/html5
     [:body
      [:script {:src "https://unpkg.com/htmx.org@1.9.0" :crossorigin "anonymous"}]
-     (render-todos-fragment todos)
+     (render-todos todos)
      [:form {:hx-post "/todos" :hx-target "#todos"}
       [:input {:type "text" :name "todo-name"}]
       [:input {:type "submit"}]]
