43cb9dc47a4fa89996816f623ce0e3c43f8687af
Chris James <quii@hey.com>
2023-04-17 19:26:46 +0100

    Thanks @gypsydave5

--- src/todo/domain/todo.clj
+++ src/todo/domain/todo.clj
@@ -1,7 +1,7 @@
 (ns todo.domain.todo)
 
 (defn new-todo [name]
-  {:name name :done false :id (random-uuid)})
+  {:name name :done false :id (str (random-uuid))})
 (defn new-todo-list [] [(new-todo "Learn Clojure")
                         (new-todo "Buy Gin")])
 
@@ -10,6 +10,7 @@
 
 (defn toggle [todos id]
   (do (println (str "toggling " id))
+      (println "uuid?" (uuid? id))
       (map (fn [t] (if (= (:id t) id) (assoc t :done (not (:done t))) t)) todos)))
 
 (defn remove-ids [todos]
