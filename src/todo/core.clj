f08e347d77c414496b0950c2593fa82cbb88e722
Chris James <quii@hey.com>
2023-04-18 08:12:15 +0100

    this feels slightly better than a janky global var

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -7,17 +7,11 @@
     [todo.domain.todo :as todo])
   (:gen-class))
 
-; todo: not sure how best to capture this atom-ness and servicey things outside of code for now
-; this is kinda bad, and you'll see is repeated in the handler test
-; but it's also nice to push impure stuff like this to the edges, need to have a think
-(def todos (atom (todo/new-list)))
-(def get-todos (fn [] @todos))
-(defn add-todo [description]
-  (swap! todos todo/add description))
-(defn toggle-todo [id]
-  (swap! todos todo/toggle id))
-
 (defn -main []
-  (run-server
-    (new-router get-todos add-todo toggle-todo)
-    {:port 5001}))
+  (let [todos (atom (todo/new-list))
+        get-todos (fn [] @todos)
+        add-todo (fn [desc] (swap! todos todo/add desc))
+        toggle-todo (fn [id] (swap! todos todo/toggle id))]
+    (run-server
+      (new-router get-todos add-todo toggle-todo)
+      {:port 5001})))
